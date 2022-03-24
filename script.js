f6a3ddda9aabf5fa66ae516b4f7a195edc3e52a3
Gabriel Garcia <89267342+gabito1966@users.noreply.github.com>
2022-03-24 13:37:23 -0300

    clase 05 completa

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-( () => {
-    
+import checkComplete from "./Components/checkComplete.js";
+import deleteIcon from "./Components/deleteIcon.js";
+ 
 const btn = document.querySelector("[data-form-btn]");
 const createTask = (evento) => {
     evento.preventDefault();
@@ -11,32 +12,17 @@ const createTask = (evento) => {
     input.value = "";
     //backticks
     const taskContent = document.createElement("div");
-    taskContent.appendChild(checkComplete());
+
     const titleTask = document.createElement("span");
     titleTask.classList.add("task");
     titleTask.innerText = value;
+    taskContent.appendChild(checkComplete());
     taskContent.appendChild(titleTask);
-    const content = `
-    <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    
    // task.innerHTML = content; 
-    task.appendChild(taskContent)
+    task.appendChild(taskContent);
+    task.appendChild(deleteIcon());
     list.appendChild(task);
 };
 // Arrow function o funciones anonimas
 btn.addEventListener('click', createTask);
-
-const checkComplete = () => {
-    const i = document.createElement('i');
-    i.classList.add('far', 'fa-check-square', 'icon');
-    i.addEventListener('click', completeTask);
-    return i;
-};
-
-const completeTask = (event) => {
-    const element = event.target;
-    element.classList.toggle("fas");
-    element.classList.toggle('completeIcon')
-    element.classList.toggle("far");
-};
-
-})();
