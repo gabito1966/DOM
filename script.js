6f8e5d4a7c5385e9710915cddd4c3d10ff3a4121
Gabriel Garcia <89267342+gabito1966@users.noreply.github.com>
2022-03-29 12:33:05 -0300

    clase05 completada

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-import checkComplete from "./Components/checkComplete.js";
-import deleteIcon from "./Components/deleteIcon.js";
- 
-const btn = document.querySelector("[data-form-btn]");
-const createTask = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    const value = input.value;
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement("li");
-    task.classList.add("card")
-    input.value = "";
-    //backticks
-    const taskContent = document.createElement("div");
+import { addTask } from "./components/addTask.js"
+import { displayTasks } from "./components/readTask.js";
 
-    const titleTask = document.createElement("span");
-    titleTask.classList.add("task");
-    titleTask.innerText = value;
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(titleTask);
-    
-   // task.innerHTML = content; 
-    task.appendChild(taskContent);
-    task.appendChild(deleteIcon());
-    list.appendChild(task);
-};
-// Arrow function o funciones anonimas
-btn.addEventListener('click', createTask);
+const btn = document.querySelector('[data-form-btn]');
+
+//Arrow functions o funciones anonimas
+btn.addEventListener('click', addTask);
+
+displayTasks();
